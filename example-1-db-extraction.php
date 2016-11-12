fcc5e321591777195c76d3ae35580ae8851d5a42
Dennis Snell <dennis.snell@automattic.com>
2016-11-11 22:46:30 -0700

    Extract db call

--- example-1-db-extraction.php
+++ example-1-db-extraction.php
@@ -1,21 +1,13 @@
 <?php
 
 function wcorl_grab_splines() {
-  global $wpdb;
+  $db_splines = wcorl_grab_splines_from_db();
 
-  $blog_id = get_current_blog_id();
-
-  $spline_count = $wpdb->query( $wpdb->prepare(
-    'SELECT * FROM wcorl_splines WHERE blog_id = %d', $blog_id
-  ) );
-
-  if ( ! $spline_count ) {
-    error_log( 'Error fetching splines' );
+  if ( $db_splines === false ) {
     return false;
   }
 
-  $db_splines = $wpdb->get_results();
-  foreach( $splines as $db_spline ) {
+  foreach( $db_splines as $db_spline ) {
     if ( $db_spline[ 'expiration' ] < time() ) {
       wcorl_track( array(
          'name' => 'foundExpiredSpline',
@@ -39,4 +31,21 @@ function wcorl_grab_splines() {
   ) );
 
   return $splines;
+}
+
+function wcorl_grab_splines_from_db() {
+  global $wpdb;
+
+  $blog_id = get_current_blog_id();
+
+  $spline_count = $wpdb->query( $wpdb->prepare(
+    'SELECT * FROM wcorl_splines WHERE blog_id = %d', $blog_id
+  ) );
+
+  if ( ! $spline_count ) {
+    error_log( 'Error fetching splines' );
+    return false;
+  }
+
+  return $wpdb->get_results();
 }
